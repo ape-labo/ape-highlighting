035fb3fac78b5543a987169bdd670e92e0bd44cc
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-10 11:31:22 +0900

    Version incremented to 3.1.1

--- example/example-jsx.js
+++ example/example-jsx.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const apeHighlighting = require('ape-highlighting'),
-    fs = require('fs');
+const apeHighlighting = require('ape-highlighting')
+const fs = require('fs')
 
 let highlighed = apeHighlighting.highlightJsx(
-    fs.readFileSync(require.resolve('../jsx/some-jsx.jsx'))
-);
+  fs.readFileSync(require.resolve('../jsx/some-jsx.jsx'))
+)
 
-console.log(highlighed);
+console.log(highlighed)
